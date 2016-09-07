b538615612cd8ce54fbfa9327df51839c24162b7
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-07 12:30:35 -0600

    relationship endpoints for invoices, items, and invoice items

---
+++ app/controllers/api/v1/invoices/merchants_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::Invoices::MerchantsController < ApplicationController
+  respond_to :json
+
+  def show
+    merchant = Invoice.find(params[:id]).merchant
+    respond_with merchant
+  end  
+end
