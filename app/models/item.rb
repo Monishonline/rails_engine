9a9e26289d756a1b10ef277116421a035a4e92d7
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-08 20:15:25 -0600

    refactor item BI queries

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,16 @@ class Item < ActiveRecord::Base
   def cents_to_dollars
     self.unit_price = (unit_price.to_f / 100).to_s
   end
+  
+  def self.successful_transactions
+    joins(invoices: :transactions).
+    merge(Transaction.successful_transactions)
+  end
 
   def best_day
     invoice_items.
-      joins(invoice: [:transactions]).
-      where(transactions: {result: "success"}).
+      joins(invoice: :transactions).
+      merge(Transaction.successful_transactions).
       group("invoices.created_at").
       order("sum_invoice_items_quantity_all_invoice_items_unit_price DESC").
       limit(1).
@@ -21,8 +26,7 @@ class Item < ActiveRecord::Base
 
   def self.most_items(num_of_items)
     select("items.*, SUM(invoice_items.quantity) AS most_items").
-      joins(invoices: :transactions).
-      where(transactions: {result: "success"}).
+      successful_transactions.
       group("items.id").
       order("most_items DESC").
       limit(num_of_items)
@@ -30,8 +34,7 @@ class Item < ActiveRecord::Base
 
   def self.most_revenue(quantity)
     select("items.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS total_revenue").
-      joins(invoices: [:transactions, :invoice_items]).
-      where(transactions: {result: "success"}).
+      successful_transactions.
       group("items.id").
       order("total_revenue DESC").
       take(quantity)
