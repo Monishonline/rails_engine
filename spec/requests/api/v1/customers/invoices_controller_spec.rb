ad272cde285b586eb18491ba3a1169eb2952a22e
Roberta Doyle <ro.scalabrin@gmail.com>
2016-09-09 01:02:55 -0600

    add customer controller tests

---
+++ spec/requests/api/v1/customers/invoices_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Customers::InvoicesController, :type => :request do
+
+  describe "GET #index" do
+    it "returns all the invoices associated with a specific customer" do
+      customer  = create(:customer, id: 1)
+      customer2 = create(:customer, id: 2)
+      invoice   = create(:invoice, id: 1, customer_id: customer.id)
+      invoice2  = create(:invoice, id: 2, customer_id: customer.id)
+      invoice2  = create(:invoice, id: 3, customer_id: customer2.id)
+
+      get '/api/v1/customers/1/invoices'
+      expect(response).to be_success
+
+      content = JSON.parse(response.body)
+      expect(content.count).to eq(2)
+    end
+  end
+end
